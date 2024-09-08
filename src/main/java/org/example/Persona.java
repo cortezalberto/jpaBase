2320df4b5d6816e7218fde83ae3b7ade9dd00bbe
Alberto Cortez <cortezalberto@gmail.com>
2024-09-08 10:01:48 -0300

    Update Persona.java

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -1,50 +1,25 @@
 package org.example;
 
 
-import javax.persistence.*;
+import lombok.*;
 
+import javax.persistence.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 @Entity
 @Table(name = "Persona")
 public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private int age;
+    private String nombre;
+    private int edad;
 
     // Constructor, getters y setters
 
 
-    public Persona(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public Persona() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
